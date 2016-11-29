a2f23c133a1288bfb17fbad5b754c58636e26153
Prizrenali <rilind.prizreni@gmail.com>
2016-11-29 09:15:39 +0100

    Beschädigung II behoben

--- basic/web/index.php
+++ basic/web/index.php
@@ -9,8 +9,4 @@ require(__DIR__ . '/../vendor/yiisoft/yii2/Yii.php');
 
 $config = require(__DIR__ . '/../config/web.php');
 
-<<<<<<< HEAD
 (new yii\web\Application($config))->run();
-=======
-(new yii\web\Application($config))->run();
->>>>>>> 6c0d9009d6b67985881e31d780380173f3c4596e
