9bd6e427675d65971d788b8a7d3db01fbd00a90d
kwlski <aweinbeck@outlook.com>
2016-11-29 09:25:08 +0100

    modified:   basic/web/index.php

--- basic/web/index.php
+++ basic/web/index.php
@@ -10,9 +10,6 @@ require(__DIR__ . '/../vendor/yiisoft/yii2/Yii.php');
 $config = require(__DIR__ . '/../config/web.php');
 
 (new yii\web\Application($config))->run();
-<<<<<<< HEAD
 
 (new yii\web\Application($config))->run();
 
-=======
->>>>>>> a2f23c133a1288bfb17fbad5b754c58636e26153
