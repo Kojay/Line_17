7fc8eb8f08bf50513d8e60db58983a7ab87cdab3
kwlski <aweinbeck@outlook.com>
2016-11-30 16:27:24 +0100

    Liste von Artikeln bearbeitet, verlinken von Sites in Liste(artikel bearbeiten)

---
+++ basic/views/site/artikel.php
@@ -0,0 +1,18 @@
+<?php
+
+/* @var $this yii\web\View */
+
+use yii\helpers\Html;
+
+$this->title = 'Artikel bearbeiten';
+$this->params['breadcrumbs'][] = $this->title;
+?>
+<div class="site-statistic">
+    <h1><?= Html::encode($this->title) ?></h1>
+
+    <p>
+        This is the Statictics page. You may modify the following file to customize its content:
+    </p>
+
+    <code><?= __FILE__ ?></code>
+</div>
