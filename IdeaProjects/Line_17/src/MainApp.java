59aeb76a145cee242c58caa0ded6b31aeb2ea9bd
roman.ott <roman.ott@students.fhnw.ch>
2017-04-24 16:56:42 +0200

    test

---
+++ IdeaProjects/Line_17/src/MainApp.java
@@ -0,0 +1,24 @@
+/**
+ * Created by Roman on 24.04.2017.
+ */
+import processing.core.PApplet;
+
+public class MainApp extends PApplet {
+
+    public static void main (String[] args ) {
+        PApplet.main("MainApp", args);
+    }
+
+    public void settings() {
+
+    }
+
+    public void setup() {
+
+    }
+
+    public void draw() {
+
+    }
+
+}
